39c10e5ca502643533c1ffb1cbc3241c35872e01
Edith Flores <Edith.Flores@activision.com>
2022-08-03 10:42:15 -0400

    Video3 Adding a new thread on my own to see it happen

--- Week4D3/Week4D3.cpp
+++ Week4D3/Week4D3.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 bool DidQuit = false;
 int ThreadVar = 0;
+int ThreadVar2 = 0;
 
 void HelloWorldThread() {
 
@@ -17,6 +18,16 @@ void HelloWorldThread() {
         }
     }
 }
+void HelloWorldThread2() {
+
+    while (!DidQuit) {
+        ThreadVar2++;
+
+        if (ThreadVar2 > 3000) {
+            ThreadVar2 = 0;
+        }
+    }
+}
 
 int main()
 {
@@ -24,14 +35,17 @@ int main()
 
     char UserInput;
     std::thread Hello(HelloWorldThread);
+    std::thread Hello2(HelloWorldThread2);
     while (!DidQuit) {
         cout << "Enter any key to display counter " << endl;
         cout << "q to quit " << endl;
         cin >> UserInput;
         DidQuit = (UserInput == 'q');
         cout << "ThreadVar: " << ThreadVar << endl;
+        cout << "ThreadVar2: " << ThreadVar2 << endl;
     }
     Hello.join();
+    Hello2.join();
 
     return 0;
 }
