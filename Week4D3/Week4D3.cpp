31396dc84f6da7e83205bc7c716b8fa269b3305d
Edith Flores <Edith.Flores@activision.com>
2022-08-03 10:37:52 -0400

    Video3

--- Week4D3/Week4D3.cpp
+++ Week4D3/Week4D3.cpp
@@ -4,15 +4,34 @@
 
 using namespace std;
 
-//Stack
+bool DidQuit = false;
+int ThreadVar = 0;
+
 void HelloWorldThread() {
-    cout << "Hello World Thread!" << endl;
+
+    while(!DidQuit) {
+        ThreadVar++;
+
+        if (ThreadVar > 1000) {
+            ThreadVar = 0;
+        }
+    }
 }
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    char UserInput;
     std::thread Hello(HelloWorldThread);
+    while (!DidQuit) {
+        cout << "Enter any key to display counter " << endl;
+        cout << "q to quit " << endl;
+        cin >> UserInput;
+        DidQuit = (UserInput == 'q');
+        cout << "ThreadVar: " << ThreadVar << endl;
+    }
     Hello.join();
+
     return 0;
 }
